c5829d9ca48b60e16eea4afc62a00ae3ec7ad342
Alisa Kapitanova <130484596+AlisaKapitanova@users.noreply.github.com>
2024-02-12 15:48:06 +0000

    newSolutions

--- 01_restaurant/challenges/01_08_findreservation_ch.sql
+++ 01_restaurant/challenges/01_08_findreservation_ch.sql
@@ -5,3 +5,12 @@
 -- Stevensen, Stephensen, Stevenson, Stephenson, Stuyvesant
 
 -- There are four people in the party. Today is June 14th.
+
+SELECT *
+FROM Customers
+WHERE LastName LIKE 'ST%'
+
+SELECT *
+FROM Reservations
+JOIN Customers ON Reservations.CustomerID = Customers.CustomerID
+WHERE LastName LIKE 'ST%' AND Date >= '2022-06-14'
--- 01_restaurant/challenges/01_09_createreservation_ch.sql
+++ 01_restaurant/challenges/01_09_createreservation_ch.sql
@@ -4,3 +4,22 @@
 -- Use the following information:
 -- Sam McAdams (smac@kinetecoinc.com), for 5 people
 -- on August 12, 2022 at 6PM (18:00)
+
+INSERT INTO Customers
+(FirstName, LastName, Email)
+VALUES ('Sam', 'McAdams', 'smac@kinetecoinc.com')
+
+INSERT INTO Reservations 
+(CustomerID, Date, PartySize)
+VALUES ( 
+(SELECT CustomerID
+FROM Customers
+WHERE LastName = 'McAdams'), 
+'2022-8-12' , 5)
+
+SELECT *
+FROM  Reservations
+WHERE Date = '2022-8-12'
+
+
+
